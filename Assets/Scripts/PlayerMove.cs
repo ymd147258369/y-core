1e73d95485c65ddc1143275a87f819badbd8f0bf
ymd147258369 <dev.AsThWzNl4XJw@gmail.com>
2018-09-15 22:17:45 +0900

    #18 プレイヤー操作のUpdateを整理
    キー入力をUpdateでとって、AddForceをFixedUpdateで実行させる

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -11,6 +11,11 @@ public class PlayerMove : MonoBehaviour
     public float QBPower = 120;
     public float JumpPower = 120;
     private Vector3 direction;
+    private Vector3 forceFowerd;
+    private Vector3 forceRight;
+
+    private bool quickBoost = false;
+    private bool jump = false;
 
     void Awake()
     {
@@ -46,10 +51,78 @@ public class PlayerMove : MonoBehaviour
             rb.AddForce(-right, ForceMode.VelocityChange);
             direction = -right;
         }
+
+
+        // 移動の物理演算
+        if (forceFowerd != Vector3.zero)
+        {
+            rb.AddForce(forceFowerd * speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (forceRight != Vector3.zero)
+        {
+            rb.AddForce(forceRight * speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        // クイックブーストの物理演算
+        if (quickBoost)
+        {
+            rb.AddForce(direction * QBPower, ForceMode.Impulse);
+            quickBoost = false;
+        }
+
+        // ジャンプの物理演算
+        if (jump)
+        {
+            jump = false;
+            rb.AddForce(JumpPower * Vector3.up * Time.deltaTime, ForceMode.Impulse);
+        }
     }
 
     void Update()
     {
+        // キー入力の受け取りと値の計算
+        // 前後移動
+        if (Input.GetKey(KeyCode.W))
+        {
+            forceFowerd = direction = this.transform.TransformDirection(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            forceFowerd = direction = -this.transform.TransformDirection(Vector3.forward);
+        }
+        else
+        {
+            forceFowerd = Vector3.zero;
+            direction = this.transform.TransformDirection(Vector3.forward);
+        }
+
+        // 左右移動
+        if (Input.GetKey(KeyCode.D))
+        {
+            forceRight = direction = this.transform.TransformDirection(Vector3.right);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            forceRight = direction = -this.transform.TransformDirection(Vector3.right);
+        }
+        else
+        {
+            forceRight = Vector3.zero;
+        }
+
+        // クイックブースト
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            quickBoost = true;
+        }
+
+        // ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             rb.AddForce(direction * QBPower, ForceMode.Impulse);
